136eaf953e00ce2f8a965ea48d750f24568acbee
Suqly Team <suqly@suqly.com>
2026-09-18 04:49:46 +0400

    fix: enable TypeORM synchronize by default to auto-create tables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
